498c03b243d62578b66edbc436ab9374f0a97cd5
Matthew Woods <mattwoods9170@gmail.com>
2020-10-07 09:17:48 -0400

    Update SearchTree.ts

--- SearchTree.ts
+++ SearchTree.ts
@@ -3,12 +3,10 @@ type node = {
 	left: node | null;
 	right: node | null;
 };
-
 function SearchTree(current: node, v: number): boolean {
 	if (current === null) {
 		return false;
 	}
-
 	if (current.value === v) {
 		return true;
 	} else if (current.value > v) {
@@ -16,11 +14,9 @@ function SearchTree(current: node, v: number): boolean {
 	}
 	return SearchTree(current.right, v);
 }
-
 function SearchList(array: number[], v: number): number {
 	let L = 0;
 	let R = array.length - 1;
-
 	while (L <= R) {
 		const idx = L + Math.floor((R - L) / 2);
 		const value = array[idx];
@@ -32,16 +28,12 @@ function SearchList(array: number[], v: number): number {
 			L = idx + 1;
 		}
 	}
-
 	return -1;
 }
-
 const list = [1, 5, 7, 9, 11, 15, 17];
-
 console.log(SearchList(list, 1));
 console.log(SearchList(list, 13));
 console.log(SearchList(list, 17));
-
 const head: node = {
 	value: 10,
 	left: {
@@ -83,7 +75,6 @@ const head: node = {
 		},
 	},
 };
-
 console.log(SearchTree(head, 1));
 console.log(SearchTree(head, 13));
 console.log(SearchTree(head, 17));
